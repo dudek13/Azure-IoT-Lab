917ac7e50aecd4c6754e62873f8fe2988fd82d80
Adrian Pieniazek <adrianpieniazek7@gmail.com>
2026-04-07 00:15:21 +0200

    Docs: Add secrets template with header guards

---
+++ secrets_example.h
@@ -0,0 +1,13 @@
+// change the filename to 'secrets.h'
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi data
+#define SECRET_WIFI_SSID "your_ssid"
+#define SECRET_WIFI_PASS "your_passwd"
+
+// Azure data
+#define SECRET_AZURE_SAS_TOKEN "your_sas"
+
+#endif
